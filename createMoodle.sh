518df924488599b7a0fd5e82e6ebe9cedb5d07f0
juanda <juandacorreo@gmail.com>
2020-05-12 14:17:43 +0200

    first boilerplate

---
+++ createMoodle.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Deberíamos recoger los siguientes datos:
+
+# MOODLE_ADMIN_USER=adminuser
+# MOODLE_ADMIN_PASSWORD=adminpassword
+# MOODLE_ADMIN_EMAIL=example@example.com
+# MOODLE_LANG=en
+# MOODLE_SITE_NAME=MySite
+# MOODLE_SITE_FULLNAME=This is my awesome site
+
+# deberíamos generar un usuario de conexión a bbdd y un nombre en base al nombre del centro
+# y una contraseña aleatoria
+# MOODLE_DB_HOST=db
+# MOODLE_DB_NAME=moodle
+# MOODLE_MYSQL_USER=dbuser
+# MOODLE_MYSQL_PASSWORD=dbpassword
+
+# una vez hecho el deploy deberíamos mandar un correo al MOODLE_ADMIN_EMAIL????
