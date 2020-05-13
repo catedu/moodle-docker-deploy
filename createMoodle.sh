65b55536799b22186bd68f252fc44aa03037307d
juanda <juandacorreo@gmail.com>
2020-05-13 23:15:06 +0200

    remove ddbb

--- createMoodle.sh
+++ createMoodle.sh
@@ -11,6 +11,14 @@
 
 # deberíamos generar un usuario de conexión a bbdd y un nombre en base al nombre del centro
 # y una contraseña aleatoria
+
+export $(cat .env | egrep -v '^#')
+echo $MYSQL_ROOT_PASSWORD
+# create database, user and grants 
+mysql --user="root" --password="${MYSQL_ROOT_PASSWORD}" --execute="CREATE DATABASE test DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci; CREATE USER pepito IDENTIFIED BY 'password'; GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,CREATE TEMPORARY TABLES,DROP,INDEX,ALTER ON moodle.* to 'pepito'@'%'"
+
+
+
 # MOODLE_DB_HOST=db
 # MOODLE_DB_NAME=moodle
 # MOODLE_MYSQL_USER=dbuser
