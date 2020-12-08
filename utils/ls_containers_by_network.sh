5b13ea90c6abe38d7f88b823e31c3bb79ab81249
Nacho Sancho <sannacho@gmail.com>
2020-12-08 10:51:08 +0100

    utils dir

--- utils/ls_containers_by_network.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-[ -z "$1" ] && echo "Debes indicar el nombre de una red" && exit 1
-
-docker network inspect "$1" | jq -r '.[].Containers' |  jq .[].Name |  sed -e 's/^"//' -e 's/"$//'
