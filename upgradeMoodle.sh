b9b36029e425f7ba6cd3e0e170ba4507a3ec9de7
cmartinb76 <cmartinb76@gmail.com>
2021-06-21 19:07:34 +0200

    Script para actualización de versión

--- upgradeMoodle.sh
+++ upgradeMoodle.sh
@@ -7,9 +7,8 @@ usage () {
 }
 
 showHelp () {
-    echo 'usage: upgradeMoodle.sh [-p] [-y] [-e env-update] -u "dirsite" -d "upgrade_version_template"'
+    echo 'usage: upgradeMoodle.sh [-y] [-e env-update] -u "dirsite" -d "upgrade_version_template"'
     echo "Options:"
-    echo "-p -> By default, moodle-code dir is deleted. With -p preserve this directory"
     echo "-y -> Yes all questions"
     echo "-e -> Add or modify env site variables"
     echo "-u -> site to upgrade. Only accept installdir"
@@ -20,23 +19,20 @@ showHelp () {
 }
 
 get_parameter(){
-    while getopts ":pye:u:d:h" opt; do
+    while getopts ":ye:u:d:h" opt; do
         case $opt in
-            p)
-                PRESERVE=true
-            ;;
             y)
                 YES=true
             ;;
             e)
                 ENVUPDATE="${OPTARG}"
                 [ ! -f "${ENVUPDATE}" ] && \
-                { echo "$(basename $0): Dont exist env file to update!"; usage; exit 1;}
+                { echo "$(basename $0): Does not exist env file to update!"; usage; exit 1;}
             ;;
             u)
                 WORKDIR="${OPTARG##*//}"
                 [ ! -d "${WORKDIR}" ] && \
-                { echo "$(basename $0): Dont exist Site to upgrade!"; usage; exit 1;}
+                { echo "$(basename $0): Does not exist Site to upgrade!"; usage; exit 1;}
                 WORKDIR=${WORKDIR%\/}
             ;;
             d)
@@ -83,7 +79,7 @@ up_service(){
     fi
 }
 
-yq() { docker run --rm -i -v "${PWD}":/workdir mikefarah/yq yq "$@"; }
+yq() { docker run --rm -i -v "${PWD}":/workdir mikefarah/yq "$@"; }
 
 rollback(){
     case $STEP in
@@ -127,7 +123,6 @@ rollback(){
 merge_envs(){
     # merge_envs env1 env2
     # Results in env1 with env2 differences, and print merged file o error
-    
     tmpfile=$(mktemp tmpenvs.XXXXXX)
     # Base is in general .env
     if [ -z "${1}" ] || ! [ -f  "${1}" ]; then
@@ -143,7 +138,7 @@ merge_envs(){
     
     # env customize deploy
     while read -r line; do
-        if ! [[ "$line" =~ ^$|#.* ]] && [[ "$line" =~ .*=.* ]]; then
+        if ! [[ "$line" =~ ^$|^#.* ]] && [[ "$line" =~ .*=.* ]]; then
             if grep -q "${line%%=*}=" "${tmpfile}" >/dev/null 2>&1; then
                 sed -iu "s/${line%%=*}=.*/$line/" "${tmpfile}"
             else
@@ -151,23 +146,24 @@ merge_envs(){
             fi
         fi
     done < "${2}"
-    tee ${1} < "${tmpfile}" && rm -f "${tmpfile}"
+    tee ${1} < "${tmpfile}" && echo "quien soy" && whoami && rm -f "${tmpfile}" && echo "borrado fichero temporal ${tmpfile}"
 }
 
 ## STEPS: init, stopservice,backup,template, end
 STEP="init"
 
 # Parameters
-PRESERVE=false
 YES=false
 MOODLECODEDIR="moodle-code"
+OLDMOODLECODEDIR="oldmoodlecode"
 get_parameter "$@"
 # WORKDIR -> Site Directory || TEMPLATEUDIR -> New template to apply
 trap 'rollback' INT TERM EXIT
 
 # Define Backup dir dinamically by date. Change if its necessary
 BACKUPDIR="/var/backup_upgrade/$(date +%Y-%m-%d--%H-%M)__${WORKDIR}"
-sudo mkdir -p "${BACKUPDIR}" && chown debian:debian "${BACKUPDIR}" || { echo "$(basename $0) - init: Problems to create ${BACKUPDIR} backup"; exit 1; }
+sudo mkdir -p "${BACKUPDIR}" && sudo chown debian:debian "${BACKUPDIR}" || { echo "$(basename $0) - init: Problems to create ${BACKUPDIR} backup"; exit 1; }
+
 
 # # Load general .env for run backup
 # set -a; [ -f .env ] && . .env; set +a
@@ -175,19 +171,25 @@ sudo mkdir -p "${BACKUPDIR}" && chown debian:debian "${BACKUPDIR}" || { echo "$(
 # Load WORKDIR .env (override general values)
 set -a; [ -f "${WORKDIR}/.env" ] && . "${WORKDIR}/.env"; set +a
 
-NEWVERSION=$(yq r "${TEMPLATEUDIR}/docker-compose.yml" services.moodle.image | cut -d: -f2 | cut -d- -f1)
+# NEWVERSION=$(yq r ${TEMPLATEUDIR}/docker-compose.yml services.moodle.image | cut -d: -f2 | cut -d- -f1)
+NEWVERSION=$(yq e .services.moodle.image ${TEMPLATEUDIR}/docker-compose.yml | cut -d: -f2 | cut -d- -f1)
 
-[[ "${VERSION}" > "${NEWVERSION}" ]] && \
-{ echo "Do you want to downgrade moodle? WTF??"; exit 1; }
-[[ "${VERSION}" = "${NEWVERSION}" ]] || \
-( $YES || (read -r -p "Do you want to make UPRGADE? Its not a UPDATE...[s/N] " RESP && [[ "$RESP" =~ ^([sS]|[sS][iI]|[yY][eE][sS]|[yY])$ ]] )) || exit 0
+echo "actual version ${VERSION}"
+echo "new version ${NEWVERSION}"
 
+dpkg --compare-versions ${VERSION} gt ${NEWVERSION} && \
+ { echo "Do you want to downgrade moodle? WTF??"; exit 1; }
+
+dpkg --compare-versions ${VERSION} eq ${NEWVERSION} && \
+{ echo "This is an upgrade not an update"; exit 1; }  
+ 
 install_pkg mariadb-client rsync
 
+
 ## Stopservice
-if (cd "${WORKDIR}" && docker-compose stop web moodle && echo "Y" | docker-compose rm web moodle); then
+if (cd "${WORKDIR}" && docker-compose stop web moodle redis && echo "Y" | docker-compose rm web moodle redis); then
     echo "$(basename $0) - stop services: Deploy ${WORKDIR} DOWN!"
-    elif (cd "${WORKDIR}" && [ -z "$(docker-compose ps -q)" ] ); then
+elif (cd "${WORKDIR}" && [ -z "$(docker-compose ps -q)" ] ); then
     # Service stopped before
     echo "$(basename $0) - stop services: DEPLOY ${WORKDIR} DOWN BEFORE!"
     ( $YES || (read -r -p "Do you want to continue UPDATE?...[s/N] " RESP && [[ "$RESP" =~ ^([sS]|[sS][iI]|[yY][eE][sS]|[yY])$ ]] )) || exit 1
@@ -205,27 +207,34 @@ echo "$(basename $0) - Backup Files..."
 sudo rsync -a "${WORKDIR%\/}" ${BACKUPDIR} || { echo "$(basename $0) - backup: Backup Files ${WORKDIR} FAIL!"; exit 1; }
 STEP="backup"
 
+
 ## Template
-# Delete moodle-code or not
-if ! $PRESERVE && ( $YES || (read -r -p "Delete ${MOODLECODEDIR}? [s/N] " RESP && [[ "$RESP" =~ ^([sS]|[sS][iI]|[yY][eE][sS]|[yY])$ ]] )); then
-    sudo rm -rf "${WORKDIR:?}/${MOODLECODEDIR}" && echo "$(basename $0) - Clean: ${MOODLECODEDIR} Deleted !" || \
-    { echo "$(basename $0) - Clean: ${MOODLECODEDIR} Deleted FAIL!"; exit 1; }
-fi
+# Move moodle-code to oldmoodlecode
+sudo mkdir "${WORKDIR:?}/${OLDMOODLECODEDIR}" 
+sudo cp "${WORKDIR:?}/${MOODLECODEDIR}/config.php" "${WORKDIR:?}/${OLDMOODLECODEDIR}/config.php"
+sudo rm -rf "${WORKDIR:?}/${MOODLECODEDIR}" && echo "$(basename $0) - Clean: ${MOODLECODEDIR} Moved !" || \
+    { echo "$(basename $0) - Clean: ${MOODLECODEDIR} Moved FAIL!"; exit 1; }
+
+
 # Upgrade skel
 rsync -av --copy-links "${TEMPLATEUDIR}"/ "${WORKDIR}" || { echo "$(basename $0) - template: Copy upgrade ${WORKDIR} FAIL!"; exit 1; }
+
+#echo " paso 8: copio config.php del viejo al nuevo2"
+# Copy config.php al original
+#sudo cp "${WORKDIR:?}/${OLDMOODLECODEDIR}/config.php" "${WORKDIR:?}/${MOODLECODEDIR}"
+#sudo rm -rf "${WORKDIR:?}/${OLDMOODLECODEDIR}"
+
+
 # Upgrade new general variables if its indicated
 [ -n "${ENVUPDATE+x}" ] && merge_envs "${WORKDIR}/.env" "${ENVUPDATE}" > /dev/null
+
 STEP="template"
 
-## Upgrade .env file with new INSTALL_TYPE before up services
-if [[ "${VERSION}" = "${NEWVERSION}" ]]; then
-    # update
-    sed  -iu --follow-symlinks 's/INSTALL_TYPE.*/INSTALL_TYPE=update/g' "${WORKDIR}/.env"
-else
-    # upgrade
-    sed  -iu --follow-symlinks 's/INSTALL_TYPE.*/INSTALL_TYPE=upgrade/g' "${WORKDIR}.env"
-fi
+## Upgrade .env file with new INSTALL_TYPE=upgrade before up services
+echo "U P G R A D E"
+sed  -iu --follow-symlinks 's/INSTALL_TYPE.*/INSTALL_TYPE=upgrade/g' "${WORKDIR}/.env"
 
+echo "cambiando la version a ${NEWVERSION} en ${WORKDIR}"
 sed  -iu --follow-symlinks "s/VERSION.*/VERSION=${NEWVERSION}/g" "${WORKDIR}/.env"
 
 # Load  .env (changes)
