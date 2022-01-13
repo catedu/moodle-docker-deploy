4d01fd1b578ce688a797609d84690047adb97443
Pablo Ruiz <pabloruizsoria@gmail.com>
2022-01-13 11:35:33 +0100

    Elimino archivo con clave de email vieja

--- template-apache-3.7.6/init-scripts/themes/fpdist/itainnova_tool/secret/credenciales.php
+++
@@ -1,6 +0,0 @@
-<?php
-$mail->Username = "admin.moodlefpdistancia@aragon.es";
-$mail->Password = "DG.GSb3J";
-$mail->From = "admin.moodlefpdistancia@aragon.es";
-$mail->FromName = "Administrador de Plataforma FP";
-?>
