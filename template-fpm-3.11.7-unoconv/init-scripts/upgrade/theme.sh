b764a00e2854d59253d05856be504f1871ccf0fe
Cristina Martín Bruna <cmartinb76@gmail.com>
2022-06-09 10:35:59 +0200

    mypublic.mustache change is no longer necessary

--- template-fpm-3.11.7-unoconv/init-scripts/upgrade/theme.sh
+++ template-fpm-3.11.7-unoconv/init-scripts/upgrade/theme.sh
@@ -5,8 +5,8 @@ echo >&2 "Updating footer.mustache..."
 cp /init-scripts/themes/footer.mustache /var/www/html/theme/moove/templates
 
 # Fixing error with contacts at moove
-echo >&2 "Updating mypublic.mustache..."
-cp /init-scripts/themes/mypublic.mustache /var/www/html/theme/moove/templates
+# echo >&2 "Updating mypublic.mustache..."
+# cp /init-scripts/themes/mypublic.mustache /var/www/html/theme/moove/templates
 
 #Updating Escolar typography
 echo >&2 "Updating Escolar typography..."
