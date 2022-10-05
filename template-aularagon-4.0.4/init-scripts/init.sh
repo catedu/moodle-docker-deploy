36be168dcf86cde015f50efb5f18ff239c86a9b4
deleyva <jesuslopezdeleyva@gmail.com>
2022-10-05 13:06:05 +0200

    añado template para aularagon 4.0.4

---
+++ template-aularagon-4.0.4/init-scripts/init.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# This script contains commands that should be executed first 
+# time the containers goes up or after upgrades to update database
+
+
+# Load sensitive data or configurable data from a .env file
+#export $(grep -E -v '^#' /init-scripts/.env | xargs)
+
+
+FILES="/init-scripts/upgrade/moodle.sh"
+
+for f in $FILES
+do
+	# after installation we disable this files so no exec is done after container restart
+	if [ -x "$f" ]; then
+		echo >&2 "$f executing..."
+		$f
+		echo >&2 "$f executed!"
+	else
+		echo >&2 "$f skipped, no x permission"
+	fi
+done
+
+#TODO import moodle book
+#TODO config repositories
+#TODO install language packages and tune them
+
+echo All done
