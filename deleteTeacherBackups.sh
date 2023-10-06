fd267c5e4ce160d9bd1c8d9e95a3e498709bfe91
Cristina Martín Bruna <cmartinb76@gmail.com>
2023-10-06 11:32:45 +0200

    Adding new name docker containers for some of them

--- deleteTeacherBackups.sh
+++ deleteTeacherBackups.sh
@@ -4,6 +4,7 @@ for SITE in *.es;
 do       
 	[ "$SITE" = "aeducar.es" ] && continue; [ "$SITE" = "moodle.catedu.es" ] && continue; [ "$SITE" = "csv-aeducar" ] && continue;
 	cd $SITE;
+	
 	AEDUCAR="$SITE"; AEDUCARMOODLE=${AEDUCAR//.}"_moodle_1";  
 	docker exec  "$AEDUCARMOODLE" moosh -n file-list "mimetype='application/vnd.moodle.backup'" > listabackups.txt; 
         cut -f1  listabackups.txt | tail -n +2 > listaids.txt;
@@ -11,5 +12,14 @@ do
 	docker exec "$AEDUCARMOODLE" moosh -n file-delete $ListaIDS;
 	docker exec "$AEDUCARMOODLE" moosh -n file-delete --flush;
 	rm listabackups.txt listaids.txt
+	
+	AEDUCAR="$SITE"; AEDUCARMOODLE=${AEDUCAR//.}"-moodle-1";  
+	docker exec  "$AEDUCARMOODLE" moosh -n file-list "mimetype='application/vnd.moodle.backup'" > listabackups.txt; 
+        cut -f1  listabackups.txt | tail -n +2 > listaids.txt;
+	ListaIDS=$(paste -s -d ' ' listaids.txt);
+	docker exec "$AEDUCARMOODLE" moosh -n file-delete $ListaIDS;
+	docker exec "$AEDUCARMOODLE" moosh -n file-delete --flush;
+	rm listabackups.txt listaids.txt
+	
 	cd ..;
 done
