4a9e8a63537839242bf8f0fee3a78a9b05089fd2
Cristina Martín Bruna <cmartinb76@gmail.com>
2023-12-01 12:48:19 +0100

    Nueva template para 4.1.6

---
+++ template-fpm-4.1.6-unoconv/init-scripts/themes/fpdist/politica-privacidad.php
@@ -0,0 +1,35 @@
+!DOCTYPE html>
+    <html  dir="ltr" lang="es" xml:lang="es">
+    <head>
+        <title>FP a distancia - Aragón</title>
+        <link rel="shortcut icon" href="https://www.adistanciafparagon.es/pluginfile.php/1/theme_moove/favicon/1615997395/FAVICON11.ico" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="keywords" content="moodle, FP a distancia - Aragón" />
+        <link rel="stylesheet" type="text/css" href="https://www.adistanciafparagon.es/theme/yui_combo.php?rollup/3.17.2/yui-moodlesimple-min.css" /><script id="firstthemesheet" type="text/css">/** Required in order to fix style inclusion problems in IE with YUI **/</script><link rel="stylesheet" type="text/css" href="https://www.adistanciafparagon.es/theme/styles.php/moove/1615997395_1/all" />
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+        <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+        <link rel="stylesheet" href="https://www.adistanciafparagon.es/theme/moove/style/aragon/aragon-wrapper.css" type="text/css">
+    </head>
+    <body  id="page-site-index" class="format-site course path-site chrome dir-ltr lang-es yui-skin-sam yui3-skin-sam test-adistanciafparagon-es pagelayout-frontpage course-1 context-2 notloggedin ">
+        <section  class="ita-sectionguia aragob_header_wrapper">
+            <!-- aragob bar  -->
+            <div class="aragob_header"></div>
+            <!-- fin aragon bar -->
+        </section>
+        <nav class="navbar nav-inicio">
+        </nav>
+        <div id="page" class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <p>El órgano responsable del tratamiento de los datos personales recogidos es laDirección General de Innovación y Formación Profesional.</p>
+                    <p>La finalidad de este tratamiento es la gestión de los proyectos desarrollados en formación profesional, así como las reclamaciones en vía administrativa y judicial y las solicitudes en vía de transparencia.</p>
+                    <p>La licitud de este tratamiento de los datos es necesaria para el cumplimiento de un interés público o ejercicio de poderes públicos aplicable al responsable del tratamiento. No se comunicarán datos a terceros, salvo obligación legal.</p>
+                    <p>Los interesados podrán ejercer sus derechos de acceso, rectificación, supresión y portabilidad de datos, así como de limitación y oposición a su tratamiento, así como a no ser objeto de decisiones individuales automatizadasa través de la sede electrónica de la Administración de la ComunidadAutónoma de Aragón con los formularios normalizados disponibles.</p>
+                    <p>Se podrá consultar la información adicional y detallada sobre esta actividad de tratamiento en <a href="https://aplicaciones.aragon.es/notif_lopd_pub/details.action?fileId=471">https://aplicaciones.aragon.es/notif_lopd_pub/details.action?fileId=471</a></p>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
